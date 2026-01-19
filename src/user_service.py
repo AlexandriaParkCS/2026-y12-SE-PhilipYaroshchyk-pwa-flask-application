8528fc815cf9dd42c0933371b80173b424080206
Philip Yaroshchyk <philip.yaroshchyk@education.nsw.gov.au>
2026-01-19 20:54:01 +1100

    added account loggin and storing user in session html files

---
+++ src/user_service.py
@@ -0,0 +1,46 @@
+from sqldb import SqlDb
+import bcrypt
+import sqlite3
+
+
+# business logic related to user account here
+class UserService:
+
+    def __init__(self, db: SqlDb):
+        self.db = db
+        # self.salt = bcrypt.gensalt()
+        self.salt = b'$2b$12$pXlzeoRw6XKXNyBbufFrYu'
+
+    # checks if user exists. if does not exist, creates a new users saving username, 
+    # email and hashed password
+    def signup(self, username, email, password):
+        print(f"signing up new user: {username}, {email}")
+        try:
+            password_hash = bcrypt.hashpw(password.encode('utf-8'), self.salt)
+            self.db.create_user(username, email, password_hash)
+        except sqlite3.IntegrityError:
+            return False
+        return True
+    
+
+    def login(self, username, password):
+        try:
+            password_hash = bcrypt.hashpw(password.encode('utf-8'), self.salt)
+            user = self.db.get_user_by_username(username)
+            if user:
+                if user["password_hash"] == password_hash:
+                    return True
+                else:
+                    print("password incorrect")
+                    return False
+        except sqlite3.IntegrityError:
+            print(f"database error, failed to login user {username}")        
+            return False
+
+
+
+            
+
+
+
+        
